facfbc774eb84e1b10bbe145285af0d394224726
Paul Suarez <paulsblogging.com@gmail.com>
2018-09-07 13:28:30 -0700

    setting up

---
+++ app/src/main/java/com/example/paulsuarez/a44_async_task/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paulsuarez.a44_async_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
